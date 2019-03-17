27586fb812940e0bfb6eccbf9ee663a119165533
chemxcode <chemxcode@gmail.com>
2019-03-17 11:41:50 +0800

    优化项目目录结构

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>我在家</div>
+</template>
+<script>
+export default {
+    name: 'Home' 
+}
+</script>
+
+
---
+++ src/pages/list/List.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>我在list
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'list' 
+}
+</script>
+
+
