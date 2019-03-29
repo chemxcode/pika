1652b4cb783a97f872ecc1d3ab5951891f67ad04
chemxcode <chemxcode@gmail.com>
2019-03-29 12:27:06 +0800

    add 预览地址

--- vue.config.js
+++ vue.config.js
@@ -26,22 +26,6 @@ module.exports = {
       .set('@', resolve('src'))
       .set('common', resolve('src/common'))
   },
-
-  // resolve: {
-  //   // 别名配置
-  //   // configuration options
-  //   'common': resolve('src/common')
-
-  // }
 }
 
-// // 项目的主要配置文件
-// module.exports = {
-//   // webpack 配置进行更细粒度的修改  https://cli.vuejs.org/zh/config/#chainwebpack
-//   chainWebpack: (config) => {
-//     //修改文件引入自定义路径
-//     config.resolve.alias
-//       .set('@', resolve('src'))
-//       .set('style', resolve('src/assets/style'))
-//   }
-// }
+
